5f4546447049ff4cc01622ffeff9a187bb9ed997
Erick De Santiago <erick.desantiago@wizeline.com>
2020-10-05 13:23:42 -0500

    feat: Go to Video when click on Card

--- src/components/CardVideo/CardVideo.component.tsx
+++ src/components/CardVideo/CardVideo.component.tsx
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import FavoriteIcon from '@material-ui/icons/FavoriteBorder';
 import FavoritedIcon from '@material-ui/icons/Favorite';
+import { useHistory } from 'react-router-dom';
 
 import image from '../../resources/image.jpg';
 
@@ -16,19 +17,26 @@ import { useApp } from '../../providers/App';
 function CardVideo({ imageSrc, title, description, id, favorited = false }: IProps) {
   const [isFavorite, setIsFavorite] = useState<boolean>(favorited);
   const { saveFavorite, removeFavorite } = useApp();
+  const history = useHistory();
   return (
     <Grid item xs={4}>
-      <Card>
+      <Card
+        onClick={() => {
+          history.push(`/video/${id}`);
+        }}
+      >
         {isFavorite ? (
           <FavoritedIcon
-            onClick={() => {
+            onClick={(evt) => {
+              evt.stopPropagation();
               setIsFavorite(false);
               removeFavorite(id);
             }}
           />
         ) : (
             <FavoriteIcon
-              onClick={() => {
+              onClick={(evt) => {
+                evt.stopPropagation();
                 setIsFavorite(true);
                 saveFavorite({ title, description, id, imgSrc: imageSrc });
               }}
