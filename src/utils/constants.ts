1279d201196324ff8d03e3bd850acd1fb4f2e017
Erick De Santiago <erick.desantiago@wizeline.com>
2020-10-05 10:31:01 -0500

    feat: Added save/remove favorites API methods

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,6 @@
 const AUTH_STORAGE_KEY = 'wa_cert_authenticated';
+const FAVORITES = 'favorites';
+const HOME_URL = '/';
+const FAVORITES_URL = '/favorites';
 
-export { AUTH_STORAGE_KEY };
+export { AUTH_STORAGE_KEY, FAVORITES, HOME_URL, FAVORITES_URL };
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,5 +1,23 @@
-const storage = {
-  get(key: string) {
+import { FAVORITES } from './constants';
+import { VideoItem } from './types';
+
+interface Video {
+  title: string;
+  id: string;
+  imgSrc: string;
+  description: string;
+}
+
+interface Storage {
+  get: (key: string) => Object | null;
+  set: (key: string, value: Object | boolean) => void;
+  getVideos: () => Video[];
+  saveVideo: (video: Video) => void;
+  removeVideo: (videoId: string) => void;
+}
+
+const storage: Storage = {
+  get(key) {
     try {
       const rawValue = window.localStorage.getItem(key);
       if (rawValue) return JSON.parse(rawValue);
@@ -10,8 +28,24 @@ const storage = {
     }
   },
 
-  set(key: string, value: JSON | boolean) {
+  set(key, value) {
     window.localStorage.setItem(key, JSON.stringify(value));
+  },
+
+  getVideos() {
+    return this.get(FAVORITES) || [];
+  },
+
+  saveVideo(video: Video) {
+    const videos: Video[] = this.getVideos();
+    videos.push(video);
+    this.set(FAVORITES, videos);
+  },
+
+  removeVideo(videoId: string) {
+    const videos: Video[] = this.getVideos();
+    const filteredVideos = videos.filter((video) => video.id !== videoId);
+    this.set(FAVORITES, filteredVideos);
   },
 };
 
